c2245b98fbd38f388c188dd26e811d8615c46c34
guangzzhang <guangzzhang@tencent.com>
2021-08-19 16:01:38 +0800

    网格化功能测试

--- dist/DynamicCreateElement.js
+++ dist/DynamicCreateElement.js
@@ -1,6 +1,9 @@
 "use strict";
 
 exports.__esModule = true;
+exports.transferStep = transferStep;
+exports.transferPixel = transferPixel;
+exports.transferShape = transferShape;
 exports["default"] = void 0;
 
 var _react = _interopRequireWildcard(require("react"));
@@ -29,6 +32,20 @@ function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.crea
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
+function transferStep(exactLength, unitLength, space) {
+  var step = Math.round(exactLength / unitLength);
+  return [step, transferPixel(step, unitLength) + space];
+}
+
+function transferPixel(step, unitLength) {
+  return unitLength * step;
+}
+
+function transferShape(exactLength, unitLength, space) {
+  var step = Math.round(exactLength / unitLength);
+  return [step === 0 ? step + 1 : step, transferPixel(step, unitLength - space) + space * (step - 1)];
+}
+
 var DynamicCreateElement = /*#__PURE__*/function (_Component) {
   _inheritsLoose(DynamicCreateElement, _Component);
 
@@ -36,6 +53,35 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
     var _this;
 
     _this = _Component.call(this, _props) || this;
+
+    _this.initGrid = function () {
+      if (!_this.props.grid) {
+        return;
+      }
+
+      try {
+        var _this$props$grid = _this.props.grid,
+            columns = _this$props$grid.columns,
+            rowHeight = _this$props$grid.rowHeight,
+            _this$props$grid$spac = _this$props$grid.space,
+            space = _this$props$grid$spac === void 0 ? 10 : _this$props$grid$spac,
+            _this$props$grid$free = _this$props$grid.freezeWidth,
+            freezeWidth = _this$props$grid$free === void 0 ? 0 : _this$props$grid$free;
+        var contentWidth = freezeWidth ? freezeWidth : _reactDom["default"].findDOMNode(_this.outerContentRef.current).offsetWidth;
+        var colWidth = Math.floor((contentWidth - (columns + 1) * space) / columns);
+
+        _this.setState({
+          gridConfig: {
+            rowHeight: rowHeight,
+            colWidth: colWidth,
+            space: space,
+            contentWidth: contentWidth
+          }
+        });
+      } catch (e) {
+        throw new Error("初始化DCE网格失败！");
+      }
+    };
 
     _this.generateKey = function (key) {
       if (key) {
@@ -67,23 +113,59 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
         return;
       }
 
-      _this.setState({
-        curStep: _api.CREATE_STATE.START,
-        startX: e.nativeEvent.clientX,
-        startY: e.nativeEvent.clientY
-      }, function () {
-        var ghostDOM = _reactDom["default"].findDOMNode(_this.ghostRef.current);
+      if (!_this.props.grid) {
+        _this.setState({
+          curStep: _api.CREATE_STATE.START,
+          startX: e.nativeEvent.clientX,
+          startY: e.nativeEvent.clientY
+        }, function () {
+          var ghostDOM = _reactDom["default"].findDOMNode(_this.ghostRef.current);
 
-        var _ghostDOM$getBounding = ghostDOM.getBoundingClientRect(),
-            x = _ghostDOM$getBounding.x,
-            y = _ghostDOM$getBounding.y;
+          var _ghostDOM$getBounding = ghostDOM.getBoundingClientRect(),
+              x = _ghostDOM$getBounding.x,
+              y = _ghostDOM$getBounding.y;
 
-        ghostDOM.style.transform = "translate(" + (_this.state.startX - x) + "px, " + (_this.state.startY - y) + "px)";
-      });
+          ghostDOM.style.transform = "translate(" + (_this.state.startX - x) + "px, " + (_this.state.startY - y) + "px)";
+        });
+      } else {
+        var _this$state$gridConfi = _this.state.gridConfig,
+            colWidth = _this$state$gridConfi.colWidth,
+            space = _this$state$gridConfi.space,
+            rowHeight = _this$state$gridConfi.rowHeight;
+
+        var _ReactDOM$findDOMNode = _reactDom["default"].findDOMNode(_this.outerContentRef.current).getBoundingClientRect(),
+            x = _ReactDOM$findDOMNode.x,
+            y = _ReactDOM$findDOMNode.y;
+
+        var _transferStep = transferStep(e.nativeEvent.clientX - x, space + colWidth, space),
+            stepX = _transferStep[0],
+            exactX = _transferStep[1];
+
+        var _transferStep2 = transferStep(e.nativeEvent.clientY - y, space + rowHeight, space),
+            stepY = _transferStep2[0],
+            exactY = _transferStep2[1];
+
+        _this.setState({
+          curStep: _api.CREATE_STATE.START,
+          startX: exactX,
+          startY: exactY,
+          g_x: stepX,
+          g_y: stepY
+        }, function () {
+          var ghostDOM = _reactDom["default"].findDOMNode(_this.ghostRef.current);
+
+          ghostDOM.style.transform = "translate(" + exactX + "px, " + exactY + "px)";
+        });
+      }
     };
 
     _this.onDrawingELement = function (e) {
-      var curStep = _this.state.curStep;
+      var _this$state = _this.state,
+          curStep = _this$state.curStep,
+          _this$state$gridConfi2 = _this$state.gridConfig,
+          space = _this$state$gridConfi2.space,
+          colWidth = _this$state$gridConfi2.colWidth,
+          rowHeight = _this$state$gridConfi2.rowHeight;
 
       if (!_this.props.active || curStep !== _api.CREATE_STATE.START && curStep !== _api.CREATE_STATE.DRAWING || e.nativeEvent.buttons !== 1) {
         return;
@@ -99,8 +181,30 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
 
       var ghostDOM = _reactDom["default"].findDOMNode(_this.ghostRef.current);
 
-      ghostDOM.style.width = clientX - _this.state.startX + "px";
-      ghostDOM.style.height = clientY - _this.state.startY + "px";
+      if (!_this.props.grid) {
+        ghostDOM.style.width = clientX - _this.state.startX + "px";
+        ghostDOM.style.height = clientY - _this.state.startY + "px";
+      } else {
+        var _ReactDOM$findDOMNode2 = _reactDom["default"].findDOMNode(_this.outerContentRef.current).getBoundingClientRect(),
+            x = _ReactDOM$findDOMNode2.x,
+            y = _ReactDOM$findDOMNode2.y;
+
+        var _transferShape = transferShape(clientX - x - _this.state.startX, space + colWidth, space),
+            stepX = _transferShape[0],
+            moveX = _transferShape[1];
+
+        var _transferShape2 = transferShape(clientY - y - _this.state.startY, space + rowHeight, space),
+            stepY = _transferShape2[0],
+            moveY = _transferShape2[1];
+
+        _this.setState({
+          g_w: stepX,
+          g_h: stepY
+        });
+
+        ghostDOM.style.width = moveX + "px";
+        ghostDOM.style.height = moveY + "px";
+      }
     };
 
     _this.onElementCreateFinish = function (e) {
@@ -136,7 +240,13 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
           position: "absolute",
           top: "0",
           left: "0"
-        })
+        }),
+        layout: _this.props.grid ? {
+          x: _this.state.g_x,
+          y: _this.state.g_y,
+          h: _this.state.g_h,
+          w: _this.state.g_w
+        } : null
       }); // 执行对外暴露的回调函数
 
 
@@ -145,28 +255,29 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
         vDOM: newNode
       });
 
-      if (!_this.state.preventDefault) {
+      if (!_this.isEventPrevent.current) {
         // 执行节点创建
-        var _this$state = _this.state,
-            children = _this$state.children,
-            props = _this$state.children.props;
+        var _this$state2 = _this.state,
+            children = _this$state2.children,
+            props = _this$state2.children.props;
 
         _this.setState({
           children: _objectSpread(_objectSpread({}, children), {}, {
             props: _objectSpread(_objectSpread({}, props), {}, {
-              children: [].concat(_this.fromPropsGetChildrenList(props), [newNode])
+              children: [].concat(_this.fromPropsGetChildrenList(props), [newNode]),
+              layout: [].concat(props.layout, [_objectSpread({
+                i: newNode.key
+              }, newNode.props.layout)])
             })
           })
         }, function () {
-          _this.props.onAfterElementCreate(_this.state.children);
+          _this.props.onAfterElementCreate(newNode, _this.state.children);
         });
       }
     };
 
     _this.preventDefault = function () {
-      _this.setState({
-        preventDefault: true
-      });
+      _this.isEventPrevent.current = true;
     };
 
     _this.resetGhost = function () {
@@ -188,19 +299,42 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
 
     _this.ghostRef = /*#__PURE__*/_react["default"].createRef();
     _this.outerContentRef = /*#__PURE__*/_react["default"].createRef();
+    _this.isEventPrevent = /*#__PURE__*/_react["default"].createRef();
+    _this.isEventPrevent.current = false;
     _this.state = {
       curStep: _api.CREATE_STATE.FINISH,
       startX: 0,
       startY: 0,
       children: _props.children,
-      preventDefault: false
+      gridConfig: {
+        rowHeight: 0,
+        colWidth: 0,
+        space: 10,
+        contentWidth: 0
+      },
+      g_x: 0,
+      g_y: 0,
+      g_w: 0,
+      g_h: 0
     };
     return _this;
   }
 
   var _proto = DynamicCreateElement.prototype;
 
-  _proto.componentDidMount = function componentDidMount() {};
+  _proto.componentDidMount = function componentDidMount() {
+    if (this.props.grid) {
+      this.initGrid();
+    }
+  }
+  /**
+   * grid {
+   *   columns: number 分割的列数
+   *   rowHeight: number 行数
+   *   space: number 行间距（默认10px）
+   * }
+   */
+  ;
 
   _proto.render = function render() {
     return /*#__PURE__*/_react["default"].createElement("div", {
@@ -221,7 +355,7 @@ var DynamicCreateElement = /*#__PURE__*/function (_Component) {
 
 DynamicCreateElement.defaultProps = {
   active: true,
-  rasterized: false,
+  grid: null,
   target: /*#__PURE__*/_react["default"].createElement("div", {
     style: {
       backgroundColor: "blue"
@@ -244,7 +378,7 @@ DynamicCreateElement.propTypes = {
   // 回调函数，当节点被创建后
   active: _propTypes["default"].bool,
   // 是否激活当前控件
-  __rasterized: _propTypes["default"].bool // 栅格化（保留）
+  grid: _propTypes["default"].object // 网格化
 
 };
 var _default = DynamicCreateElement;
